f37b3e7618f10816774ab475ebcd4dacf38a3a5b
Eric Einhaus <eeinhaus17@gmail.com>
2021-08-17 17:38:22 -0400

    closure implementation for useState

--- Component.js
+++ Component.js
@@ -1,7 +1,7 @@
 import { useState } from './MyReact.js';
 
 export default function Component({ propCount }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(10);
   const propCountDoubled = 0;
 
   return `
--- MyReact.js
+++ MyReact.js
@@ -4,26 +4,33 @@ const componentState = new Map();
 
 export function useState(initialState) {
   const id = globalId;
-  const { cache, props, component } = componentState.get(globalParent);
-  if (cache[id] == null) {
-    cache[id] = {
-      value: typeof initialState === 'function' ? initialState() : initialState,
-    };
-  }
-
-  const setState = (state) => {
-    if (typeof state === 'function') {
-      cache[id].value = state(cache[id].value);
-    } else {
-      cache[id].value = state;
-    }
-    render(component, props, globalParent);
-  };
-
-  const state = cache[id].value;
+  const parent = globalParent;
   globalId++;
 
-  return [state, setState];
+  //Creating a closure
+  return (() => {
+    const { cache } = componentState.get(parent);
+    if (cache[id] == null) {
+      cache[id] = {
+        value:
+          typeof initialState === 'function' ? initialState() : initialState,
+      };
+    }
+
+    const setState = (state) => {
+      const { props, component } = componentState.get(parent);
+      if (typeof state === 'function') {
+        cache[id].value = state(cache[id].value);
+      } else {
+        cache[id].value = state;
+      }
+      render(component, props, parent);
+    };
+
+    const state = cache[id].value;
+
+    return [state, setState];
+  })();
 }
 
 export function render(component, props, parent) {
